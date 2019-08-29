e8ca2a9cb07957a7834bd0ad2a8c7fdb14d7aa05
Satyam Khadka <Satyam.khadka123@gmail.com>
2019-08-29 23:41:13 +0545

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDKLX7PX82jROs6AA0EoNGL-8PyyYLYkwQ',
-    authDomain: 'order-system-satyam.firebaseapp.com',
-    databaseURL: 'https://order-system-satyam.firebaseio.com',
-    projectId: 'order-system-satyam',
-    storageBucket: 'order-system-satyam.appspot.com',
-    messagingSenderId: '524483191474',
-    appId: '1:524483191474:web:2a0f49dfaac3f1d9'
+    
   }
 };
